5d913ff1deea458bf92af717118aa6e22376cbb7
Kamenchu <iankamenchu@gmail.com>
2024-03-06 06:37:24 +0300

    completed tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [element for element in word_list if self.is_anagram(element)]
+
+    def is_anagram(self, candidate):
+        return sorted(self.word) == sorted(candidate)
